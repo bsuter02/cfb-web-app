8c747c460c575b7e73382f644dddf169936b0306
Brendan Suter <brendansuter02@gmail.com>
2025-02-18 19:40:32 -0600

    Score predictor version 1. Will be used for predicting individual games, but not full schedules. No tie breakers or neutral site implemented yet.

---
+++ score_predictor.py
@@ -0,0 +1,43 @@
+from database import database_query
+
+def predict_score(away, home):
+    
+    # Pull FPI, Offensive Efficiency, and Defensive Efficiency for each team as determined by ESPN analytics.
+    away_team = database_query.get_cfb_fpi_by_team(away)
+    home_team = database_query.get_cfb_fpi_by_team(home)
+
+    # Grabs Home Field Advantage for the team hosting, as determined in an online analysis.
+    home_hfa = database_query.get_cfb_hfa_by_team(home)
+    
+    # Average score a cfb team in 2024 scores.
+    base_total_score = 26.65
+    
+    # Calculate time of possession (TOP) ratio based on offensive and defensive efficiencies
+    home_top = float(home_team.get("OffEfficiency")) + (100 - float(away_team.get("DefEfficiency")))
+    away_top = float(away_team.get("OffEfficiency")) + (100 - float(home_team.get("DefEfficiency")))
+    total_top = home_top + away_top
+    
+    # TOP factor (1 = 30 min est.)
+    home_possession = 2* home_top / total_top
+    away_possession = 2* away_top / total_top
+
+    # TOP factor adjustment (to prevent extremely unbalanced TOP)
+    home_possession = min(0.67*2,max(home_possession,0.33*2))
+    away_possession = min(0.67*2,max(away_possession,0.33*2))
+
+    # Scoring Efficiency off vs def factor, roughly how fast a team plays against a particular defense
+    home_score_eff = (float(home_team.get("OffEfficiency")) - float(away_team.get("DefEfficiency")) + 100)/100
+    away_score_eff = (float(away_team.get("OffEfficiency")) - float(home_team.get("DefEfficiency")) + 100)/100
+
+    # Estimation of point total for the game (both teams)
+    total_score = base_total_score*home_possession*home_score_eff + base_total_score*away_possession*away_score_eff
+
+    # Score estimation by dividing the point total by 2 and adjusting based on expected margin on neutral site (via FPI) and HFA effects.
+    home_score = total_score/2 + (float(home_team.get("FPI")) - float(away_team.get("FPI")))/2 + float(home_hfa.get("HFA"))/2
+    away_score = total_score/2 + (float(away_team.get("FPI")) - float(home_team.get("FPI")))/2 - float(home_hfa.get("HFA"))/2
+    
+    return away_team.get("TeamName") + " @ " + home_team.get("TeamName") + " Prediction:\n   " +  away_team.get("TeamName") + ": " + str(round(away_score)) + "; " + home_team.get("TeamName") + ": " + str(round(home_score))
+
+# Predict game outcome Away vs Home
+predicted_scores = predict_score("Notre Dame", "Ohio State")
+print(predicted_scores)
