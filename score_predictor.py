a11f3b0502f6192d15af55f59a1fde88a41a47b2
Brendan Suter <brendansuter02@gmail.com>
2025-03-11 21:20:26 -0500

    Added prediction model for single games using SP+ rankings

--- score_predictor.py
+++ score_predictor.py
@@ -1,6 +1,6 @@
 from database import database_query
 
-def predict_score(away, home):
+def predict_score_fpi(away, home):
     
     # Pull FPI, Offensive Efficiency, and Defensive Efficiency for each team as determined by ESPN analytics.
     away_team = database_query.get_cfb_fpi_by_team(away)
@@ -38,6 +38,27 @@ def predict_score(away, home):
     
     return away_team.get("TeamName") + " @ " + home_team.get("TeamName") + " Prediction:\n   " +  away_team.get("TeamName") + ": " + str(round(away_score)) + "; " + home_team.get("TeamName") + ": " + str(round(home_score))
 
+def predict_score_sp_plus(away, home):
+    
+    # Pull FPI, Offensive Efficiency, and Defensive Efficiency for each team as determined by ESPN analytics.
+    away_team = database_query.get_cfb_sp_plus_by_team(away)
+    home_team = database_query.get_cfb_sp_plus_by_team(home)
+
+    # Grabs Home Field Advantage for the team hosting, as determined in an online analysis.
+    home_hfa = database_query.get_cfb_hfa_by_team(home)
+    
+    home_factor = ((float(home_team.get("OffPoint"))) + (float(away_team.get("DefPoint"))))/2
+    away_factor = ((float(away_team.get("OffPoint"))) + (float(home_team.get("DefPoint"))))/2
+
+    total_score = home_factor + away_factor
+
+    raw_margin = float(home_team.get("SPPlus")) - float(away_team.get("SPPlus"))
+
+    away_score = total_score/2 - raw_margin/2 - float(home_hfa.get("HFA"))/2
+    home_score = total_score/2 + raw_margin/2 + float(home_hfa.get("HFA"))/2
+    
+    return away_team.get("TeamName") + " @ " + home_team.get("TeamName") + " Prediction:\n   " +  away_team.get("TeamName") + ": " + str(round(away_score)) + "; " + home_team.get("TeamName") + ": " + str(round(home_score))
+
 # Predict game outcome Away vs Home
-predicted_scores = predict_score("Notre Dame", "Ohio State")
+predicted_scores = predict_score_sp_plus("Iowa", "Iowa State")
 print(predicted_scores)
