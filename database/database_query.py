37ffcb6eaf2b86381d4a30ee1913adacf353769c
Brendan Suter <brendansuter02@gmail.com>
2025-03-25 18:21:47 -0500

    Added methods to query info in the ESPN table

--- database/database_query.py
+++ database/database_query.py
@@ -81,3 +81,45 @@ def get_cfb_sp_plus_by_team(team):
         if connection.is_connected():
             cursor.close()
             connection.close()
+
+def get_teamname_by_nickname(team):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        query = "SELECT TeamName FROM ESPN_ID WHERE ESPN_Nickname = %s;"
+        cursor.execute(query, (team,))
+
+        results = cursor.fetchone()
+
+        return results  # Returns a dict with each field in the fpi table
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def get_id_by_teamname(team):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        query = "SELECT ESPN_ID FROM ESPN_ID WHERE TeamName = %s;"
+        cursor.execute(query, (team,))
+
+        results = cursor.fetchone()
+
+        return results  # Returns a dict with each field in the fpi table
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
